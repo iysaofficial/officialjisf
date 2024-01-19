01f1447d9a134502eabd18eafb196daa6532efdf
young <youngscientist.iysa@gmail.com>
2024-01-19 14:54:53 +0700

    new

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -25,7 +25,7 @@ const Hero = () => {
                             </h1>
                             {/* <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">We are a team of highly motivated and skilled developers dedicated to delivering only the best software.</div> */}
                             <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
-                                <a href='https://registration-jisf.vercel.app/' target='_blank' className="text-white bg-green-900 hover:bg-green-700 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
+                                <a href='https://registration-jisf-2024.vercel.app/' target='_blank' className="text-white bg-green-900 hover:bg-green-700 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
                                     Open Registration
                                     <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/sv" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                 </a>
