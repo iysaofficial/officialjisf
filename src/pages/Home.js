d646fc9daf63462b85abd24056834f1abdcf8f93
young <youngscientist.iysa@gmail.com>
2024-06-26 14:38:28 +0700

    timevenue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import Hero from '../components/Hero';
 import Intro from '../components/Intro';
 // import Portfolio from '../components/Portfolio';
 import Services from '../components/Services';
+import Timevenue from '../components/Timevenue';
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
         <>
             <Hero />
             <Intro />
+            <Timevenue />
             <Services />
             {/* <Portfolio /> */}
             <Clients />
