5fad22369bbe6cce984a159d267eacda384ac6a2
iysaofficial <110337759+iysaofficial@users.noreply.github.com>
2025-03-17 10:08:29 +0700

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -25,8 +25,8 @@ const Hero = () => {
                             </h1>
                             {/* <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">We are a team of highly motivated and skilled developers dedicated to delivering only the best software.</div> */}
                             <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
-                                <a href='/homeregist' className="text-white bg-green-900 hover:bg-green-700 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
-                                    Open Registration 2025
+                                <a href='/' className="text-white bg-green-900 hover:bg-green-700 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
+                                    Close Registration 2025
                                     <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/sv" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                 </a>
                                 {/* <Link to="/contact" className="text-white bg-green-900 hover:bg-green-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
