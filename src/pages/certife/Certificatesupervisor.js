7e665a0c85ac81094c24cb051b01cf05f3f0bc7c
iysaofficial <youngscientist.iysa@gmail.com>
2026-05-11 16:34:51 +0700

    UP SPV 2026

--- src/pages/certife/Certificatesupervisor.js
+++ src/pages/certife/Certificatesupervisor.js
@@ -33,6 +33,22 @@ const Certificatesupervisor = () => {
                     src="./assets/logo/new.jpg"
                   /> */}
                   <h2 className="font-semibold my-4 text-2xl text-center text-green-900">
+                    Certificate Supervisor 2026
+                  </h2>
+                  <p className="font-bold text-green-900">
+                    {/* Lokasi Venue: {" "}   */}
+                    <a href="https://drive.google.com/drive/folders/19gj8_tzGn4pEoLWcytgiGo9HD7dz3Xfx?usp=sharing" target="_blank" rel="noopener noreferrer">Click Here</a>
+                  </p>
+                </div>
+              </div>
+              <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                <div className="m-2 text-justify text-sm">
+                  {/* <img
+                    alt="card img"
+                    className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out"
+                    src="./assets/logo/new.jpg"
+                  /> */}
+                  <h2 className="font-semibold my-4 text-2xl text-center text-green-900">
                     Certificate Supervisor 2025
                   </h2>
                   <p className="font-bold text-green-900">
