665352d2d49c3f71d20239c04cf2fb9d8539e5d1
IYSA <youngscientist.iysa@gmail.com>
2026-08-24 14:41:26 +0700

    fix navbar guidebook

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -21,7 +21,7 @@ const NavBar = () => {
     return (
         <nav className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 ${!top && 'bg-white shadow-lg'}`}>
             <div className="flex flex-row justify-between items-center py-2">
-                <div className="flex flex-row justify-center md:px-12 md:mx-12 items-center text-center font-semibold">
+                <div className="flex flex-row justify-center md:px-4 md:mx-4 items-center text-center font-semibold">
                     <img src='./assets/logo/jisf.png' alt="JISF Logo" className='w-16 fill-current'smooth to="/#hero"/>
                     
                 </div>
@@ -36,7 +36,7 @@ const NavBar = () => {
                             )}
                         </svg>
                     </button>
-                    <div className='hidden space-x-6 lg:inline-block p-5'>
+                    <div className='hidden lg:flex lg:flex-row lg:items-center p-5'>
                         <NavLinks />
                     </div>
 
